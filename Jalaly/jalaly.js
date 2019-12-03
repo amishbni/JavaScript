bd7c2ea6c0f4f460b9dbc4cf0e427fbd230ccd8e
Amir A. Shabani <amirshabani011@gmail.com>
2019-12-03 19:40:47 +0330

    Get time from momentjs

--- Jalaly/jalaly.js
+++ Jalaly/jalaly.js
@@ -1,2 +1,4 @@
-let date_h1 = document.getElementById("date")
-date_h1.innerText = Date()
+let moment = require('moment')
+
+let date_h1 = document.getElementById("date");
+date_h1.innerText = moment().format('dddd, MMMM D, YYYY');
